acb0dae3f0314c11b02d9a455454ceca7de66bc5
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2024-02-03 10:53:42 +0530

    commit last

---
+++ src/User.jsx
@@ -0,0 +1,17 @@
+let user = {
+    user-id: "",
+    user-email: "",
+    user-given_name: "",
+    user-middle_name: "",
+    name: "",
+    family_name: "",
+    nickname: "",
+    phone_number: "",
+    comment: "",
+    picture: "",
+    directory: "",
+    meta_data: "",
+    tags: [],
+    is_suspended: "",
+  };
+  export default user;
